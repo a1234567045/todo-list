e7d4ce0371a5003b0f3059a68d25dcccda427c7f
wei <a1234567045@yahoo.com.tw>
2021-05-12 19:58:01 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo') //載入 Todo model
-const app = express()
+const bodyParser = require('body-parser')
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+const Todo = require('./models/todo') //載入 Todo model
+
+const app = express()
 
 //設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
 //取得資料庫連線
 const db = mongoose.connection
+
 //連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -21,12 +23,29 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() //取出 Todo model 裡的所有物件
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 模板
     .catch(error => console.error(error)) //錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
